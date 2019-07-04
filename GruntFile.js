321c4437613bbc3805fe65b1c40b3f6ccf7de999
Jason Rayga <>
2019-07-04 23:00:49 +0800

    Updates

--- GruntFile.js
+++ GruntFile.js
@@ -1,25 +1,25 @@
 module.exports = function (grunt) {
-    grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
-      //grunt task configuration will go here
-      ngAnnotate: {
-        options: {
-          singleQuotes: true
-        },
-        app: {
-          files: {
-            './public/min-safe/mainJS/app.js': ['./JS/app.js']
-          }
-        }
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    //grunt task configuration will go here
+    ngAnnotate: {
+      options: {
+        singleQuotes: true
       },
-      concat: {
-        js: { //target
-          src: ['./public/min-safe/app.js', './public/min-safe/mainJS/*.js'],
-          dest: './public/min/app.js'
+      app: {
+        files: {
+          './public/min-safe/mainJS/app.js': ['./JS/app.js']
         }
-      },
-      uglify: {
-       options: {
+      }
+    },
+    concat: {
+      js: { //target
+        src: ['./public/min-safe/app.js', './public/min-safe/mainJS/*.js'],
+        dest: './public/min/app.js'
+      }
+    },
+    uglify: {
+      options: {
         mangle: true,
         compress: {
           drop_console: true,
@@ -28,21 +28,21 @@ module.exports = function (grunt) {
           sequences: true,
           join_vars: true,
           dead_code: true,
-          ie8:true
+          ie8: true
         },
       },
-        js: { //target
-          src: './public/min/*.js',
-          dest: './JS/minified/alljs.min.js'
-        }
+      js: { //target
+        src: './public/min/*.js',
+        dest: './JS/minified/alljs.min.js'
       }
-    });
-  
-    //load grunt tasks
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-ng-annotate');
-  
-    //register grunt default task
-    grunt.registerTask('default', ['ngAnnotate', 'concat', 'uglify']);
-  }
+    }
+  });
+
+  //load grunt tasks
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-ng-annotate');
+
+  //register grunt default task
+  grunt.registerTask('default', ['ngAnnotate', 'concat', 'uglify']);
+}
