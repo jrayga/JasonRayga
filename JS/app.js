a3231f7a466304e49dbbfc57cce7106466f66059
Jason Rayga <raygajason@gmail.com>
2021-09-24 16:12:27 +0800

    Updates

--- JS/app.js
+++ JS/app.js
@@ -28,7 +28,6 @@ app.controller('Index', function ($scope, $anchorScroll) {
     $scope.yearNow = new Date().getFullYear();
 
     $scope.goTo = function (scrollTo) {
-        console.log("TCL: $scope.goTo -> scrollTo", scrollTo)
         $scope.nav = scrollTo;
         $anchorScroll(scrollTo);
     };
@@ -36,38 +35,37 @@ app.controller('Index', function ($scope, $anchorScroll) {
     $scope.hoveredName = function () {
         $scope.didHoverName = $scope.didHoverName === null ? false : $scope.didHoverName
         $scope.didHoverName = $scope.didHoverName === false ? true : false
-        console.log("$scope.hoveredName -> $scope.didHoverName", $scope.didHoverName)
     }
 
     $scope.worksImgAndDesc = [
         {
             title: 'RATS (Recruiters Applicant Tracking Software)',
             desc: 'Recruiters Applicant Tracking Software or RATS is the first Filipino developed Applicant Tracking Software and Resume Bank packed with powerful and innovative features and functionalities empowering every recruiter and helping every company in their hiring strategy.',
-            img: './ASSETS/img/RATSLogo.png',
+            img: 'https://raw.githubusercontent.com/jrayga/JasonRayga/master/ASSETS/img/RATSLogo.png',
             url: 'https://www.facebook.com/aomoschrs/photos/a.188396104827815/1086709888329761/'
         },
         {
             title: 'Q App',
             desc: 'A mobile application (iOS and Android) that allows commuters to virtually queue instead of standing in line in terminals just to secure a seat in public transportation',
-            img: './ASSETS/img/queue_logo.png',
+            img: 'https://raw.githubusercontent.com/jrayga/JasonRayga/master/ASSETS/img/queue_logo.png',
             url: 'https://q.inciteasia.co'
         },
         {
             title: 'DaGAT (Data Gathering & Analytics Tool)',
             desc: 'Is a web-based platform for sustainability Data Gathering made by PBE that will aid in data collection, analysis, and visualization. This will also enable the company to establish a systematic process for sustainability data management moving forward.',
-            img: './ASSETS/img/dagat-icon.png',
+            img: 'https://raw.githubusercontent.com/jrayga/JasonRayga/master/ASSETS/img/dagat-icon.png',
             url: 'https://landing.pbe-sdt.com'
         },
         {
             title: 'SDGs Our Biz Sustainability Web Tool',
             desc: 'A web-based platform to look at how Philippine Businesses are contributing to the Sustainable Development Goals',
-            img: './ASSETS/img/sdgbizphbiz_logo_blue.png',
+            img: 'https://raw.githubusercontent.com/jrayga/JasonRayga/master/ASSETS/img/sdgbizphbiz_logo_blue.png',
             url: 'https://sdgsbiz.ph/docroot/'
         },
         {
             title: 'Project Moses',
             desc: 'Project Moses is a Crisis Response Toolbox that aims to be an all-in-one web dashboard for Filipinos in times of calamity and crisis.',
-            img: './ASSETS/img/project-moses.jpg',
+            img: 'https://raw.githubusercontent.com/jrayga/JasonRayga/master/ASSETS/img/project-moses.jpg',
             url: 'https://www.projectmoses.ph/'
         }
     ];
