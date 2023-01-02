e798bf7fef2893b669a1ecfd350488e20e8e99e2
Jason Rayga <raygajason@gmail.com>
2023-01-02 08:57:21 +0800

    Minor updates

--- GruntFile.js
+++ GruntFile.js
@@ -1,29 +1,29 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON("package.json"),
 
-    clean: ["dist", '.tmp'],
+    clean: ["dist", ".tmp"],
 
     copy: {
       main: {
         expand: true,
-        cwd: 'dev/',
-        src: ['**'],
-        dest: 'dist/'
-      }
+        cwd: "dev/",
+        src: ["**"],
+        dest: "dist/",
+      },
     },
 
     useminPrepare: {
-      html: 'dev/index.html'
+      html: "dev/index.html",
     },
 
     usemin: {
-      html: ['dist/index.html']
+      html: ["dist/index.html"],
     },
 
     uglify: {
       options: {
-        report: 'min',
+        report: "min",
         mangle: false,
         compress: {
           drop_console: true,
@@ -32,35 +32,36 @@ module.exports = function (grunt) {
           sequences: true,
           join_vars: true,
           dead_code: true,
-          ie8: true
-        }
-      }
+          ie8: true,
+        },
+      },
     },
 
     cachebreaker: {
       prod: {
         options: {
-          match: ['dist/main/js/main.min.js', 'dist/main/main.min.css']
+          match: ["dist/main/js/main.min.js", "dist/main/main.min.css"],
         },
-        files: { src: ['dist/index.html'] }
-      }
+        files: { src: ["dist/index.html"] },
+      },
     },
 
     ngAnnotate: {
       options: {
-        singleQuotes: true
+        singleQuotes: true,
       },
       app: {
         files: {
-          './public/min-safe/mainJS/main-ng.js': ['./JS/app.js'],
-        }
-      }
+          "./public/min-safe/mainJS/main-ng.js": ["./JS/app.js"],
+        },
+      },
     },
     concat: {
-      js: { // Target
-        src: ['./public/min-safe/app.js', './public/min-safe/mainJS/*.js'],
-        dest: './public/min/app.js'
-      }
+      js: {
+        // Target
+        src: ["./public/min-safe/app.js", "./public/min-safe/mainJS/*.js"],
+        dest: "./public/min/app.js",
+      },
     },
     uglify: {
       options: {
@@ -72,40 +73,39 @@ module.exports = function (grunt) {
           sequences: true,
           join_vars: true,
           dead_code: true,
-          ie8: true
+          ie8: true,
         },
       },
-      js: { // Target
-        src: './public/min/*.js',
-        dest: './public/minified/all.min.js'
-      }
-    }
+      js: {
+        // Target
+        src: "./public/min/*.js",
+        dest: "./public/minified/all.min.js",
+      },
+    },
   });
 
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-usemin');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-cache-breaker');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify-es');
-  grunt.loadNpmTasks('grunt-ng-annotate');
-
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-usemin");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-cache-breaker");
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-contrib-uglify-es");
+  grunt.loadNpmTasks("grunt-ng-annotate");
 
   // Tell Grunt what to do when we type "grunt" into the terminal
-  grunt.registerTask('default', [
-    'useminPrepare',
-    'copy',
-    'concat',
-    'uglify',
-    'cssmin',
-    'usemin',
-    'cachebreaker',
-    'ngAnnotate',
-    'concat',
-    'uglify'
+  grunt.registerTask("default", [
+    "useminPrepare",
+    "copy",
+    "concat",
+    "uglify",
+    "cssmin",
+    "usemin",
+    "cachebreaker",
+    "ngAnnotate",
+    "concat",
+    "uglify",
   ]);
-
 };
--- JS/app.js
+++ JS/app.js
@@ -1,72 +1,81 @@
-var app = angular.module('app_module', ['ngRoute', 'ngAnimate']);
+var app = angular.module("app_module", ["ngRoute", "ngAnimate"]);
 app.config(function ($routeProvider, $locationProvider) {
-    // $routeProvider
-    //     .when('/',
-    //         {
-    //             controller: 'About',
-    //             templateUrl: 'partials/about.html'
-    //         })
-    //     .otherwise(
-    //         {
-    //             redirectTo: '/'
-    //         })
+  // $routeProvider
+  //     .when('/',
+  //         {
+  //             controller: 'About',
+  //             templateUrl: 'partials/about.html'
+  //         })
+  //     .otherwise(
+  //         {
+  //             redirectTo: '/'
+  //         })
 
-    // Check browser support
-    // $locationProvider.html5Mode(true); will cause an error $location in HTML5 mode requires a tag to be present! Unless you set baseUrl tag after head tag like so: <head> <base href="/">
-    // To know more about setting base URL visit: https://docs.angularjs.org/error/$location/nobase
-    // If you don't wish to set base URL then use this
-    if (window.history && window.history.pushState) {
-        $locationProvider.html5Mode({
-            enabled: true,
-            requireBase: true
-        });
-    }
+  // Check browser support
+  // $locationProvider.html5Mode(true); will cause an error $location in HTML5 mode requires a tag to be present! Unless you set baseUrl tag after head tag like so: <head> <base href="/">
+  // To know more about setting base URL visit: https://docs.angularjs.org/error/$location/nobase
+  // If you don't wish to set base URL then use this
+  if (window.history && window.history.pushState) {
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: true,
+    });
+  }
 });
-app.controller('Index', function ($scope, $anchorScroll) {
-    $scope.didHoverName = null
-    $scope.nav = "intro";
-    $scope.yearNow = new Date().getFullYear();
+app.controller("Index", function ($scope, $anchorScroll) {
+  $scope.didHoverName = null;
+  $scope.nav = "intro";
+  $scope.yearNow = new Date().getFullYear();
 
-    $scope.goTo = function (scrollTo) {
-        $scope.nav = scrollTo;
-        $anchorScroll(scrollTo);
-    };
+  $scope.goTo = function (scrollTo) {
+    $scope.nav = scrollTo;
+    $anchorScroll(scrollTo);
+  };
 
-    $scope.hoveredName = function () {
-        $scope.didHoverName = $scope.didHoverName === null ? false : $scope.didHoverName
-        $scope.didHoverName = $scope.didHoverName === false ? true : false
-    }
+  $scope.hoveredName = function () {
+    $scope.didHoverName =
+      $scope.didHoverName === null ? false : $scope.didHoverName;
+    $scope.didHoverName = $scope.didHoverName === false ? true : false;
+  };
 
-    $scope.worksImgAndDesc = [
-        {
-            title: 'RATS (Recruiters Applicant Tracking Software)',
-            desc: 'Recruiters Applicant Tracking Software or RATS is the first Filipino developed Applicant Tracking Software and Resume Bank packed with powerful and innovative features and functionalities empowering every recruiter and helping every company in their hiring strategy.',
-            img: 'https://raw.githubusercontent.com/jrayga/JasonRayga/master/ASSETS/img/RATSLogo.png',
-            url: 'https://www.linkedin.com/in/rats-global-5a79a3145/'
-        },
-        {
-            title: 'Q App',
-            desc: 'A mobile application (iOS and Android) that allows commuters to virtually queue instead of standing in line in terminals just to secure a seat in public transportation',
-            img: 'https://raw.githubusercontent.com/jrayga/JasonRayga/master/ASSETS/img/queue_logo.png',
-            url: 'https://q.inciteasia.co'
-        },
-        {
-            title: 'DaGAT (Data Gathering & Analytics Tool)',
-            desc: 'Is a web-based platform for sustainability Data Gathering made by BSD (Formerly known as PBE) that will aid in data collection, analysis, and visualization. This will also enable the company to establish a systematic process for sustainability data management moving forward.',
-            img: 'https://raw.githubusercontent.com/jrayga/JasonRayga/master/ASSETS/img/dagat-icon.png',
-            url: 'https://bsdph.org/'
-        },
-        {
-            title: 'SDGs Our Biz Sustainability Web Tool',
-            desc: 'A web-based platform to look at how Philippine Businesses are contributing to the Sustainable Development Goals',
-            img: 'https://raw.githubusercontent.com/jrayga/JasonRayga/master/ASSETS/img/sdgbizphbiz_logo_blue.png',
-            url: 'https://sdgsbiz.ph/docroot/'
-        },
-        {
-            title: 'Project Moses',
-            desc: 'Project Moses is a Crisis Response Toolbox that aims to be an all-in-one platform for Filipinos in times of calamity and crisis mainly and should still be relevant even in times of peace. It is a publicly accessible collaboration toolbox using Information Communication Technology (ICT) meant to connect crisis response actors. The tools are to meant to address the issues on lack of relevant and real-time information to help stop the wildfire of misinformation and promote information that would assuage fears, understand risk, implement effective social distancing and improve our public health.',
-            img: 'https://raw.githubusercontent.com/jrayga/JasonRayga/master/ASSETS/img/project-moses.jpg',
-            url: 'https://www.projectmoses.ph/'
-        }
-    ];
+  $scope.worksImgAndDesc = [
+    {
+      title: "RATS (Recruiters Applicant Tracking Software)",
+      desc: "Recruiters Applicant Tracking Software or RATS is the first Filipino developed Applicant Tracking Software and Resume Bank packed with powerful and innovative features and functionalities empowering every recruiter and helping every company in their hiring strategy.",
+      img: "https://raw.githubusercontent.com/jrayga/JasonRayga/master/ASSETS/img/RATSLogo.png",
+      url: "https://www.linkedin.com/in/rats-global-5a79a3145/",
+      shortDesc:
+        "RATS is the first Filipino developed Applicant Tracking Software and Resume Bank",
+    },
+    {
+      title: "Q App",
+      desc: "A mobile application (iOS and Android) that allows commuters to virtually queue instead of standing in line in terminals just to secure a seat in public transportation",
+      img: "https://raw.githubusercontent.com/jrayga/JasonRayga/master/ASSETS/img/queue_logo.png",
+      url: "https://q.inciteasia.co",
+      shortDesc:
+        "A mobile application that allows commuters to virtually queue",
+    },
+    {
+      title: "DaGAT (Data Gathering & Analytics Tool)",
+      desc: "Is a web-based platform for sustainability Data Gathering made by BSD (Formerly known as PBE) that will aid in data collection, analysis, and visualization. This will also enable the company to establish a systematic process for sustainability data management moving forward.",
+      img: "https://raw.githubusercontent.com/jrayga/JasonRayga/master/ASSETS/img/dagat-icon.png",
+      url: "https://bsdph.org/",
+      shortDesc: "Web-based platform for sustainability Data Gathering",
+    },
+    {
+      title: "SDGs Our Biz Sustainability Web Tool",
+      desc: "A web-based platform to look at how Philippine Businesses are contributing to the Sustainable Development Goals",
+      img: "https://raw.githubusercontent.com/jrayga/JasonRayga/master/ASSETS/img/sdgbizphbiz_logo_blue.png",
+      url: "https://sdgsbiz.ph/docroot/",
+      shortDesc: "Sustainable Development Goals",
+    },
+    {
+      title: "Project Moses",
+      desc: "Project Moses is a Crisis Response Toolbox that aims to be an all-in-one platform for Filipinos in times of calamity and crisis mainly and should still be relevant even in times of peace. It is a publicly accessible collaboration toolbox using Information Communication Technology (ICT) meant to connect crisis response actors. The tools are to meant to address the issues on lack of relevant and real-time information to help stop the wildfire of misinformation and promote information that would assuage fears, understand risk, implement effective social distancing and improve our public health.",
+      img: "https://raw.githubusercontent.com/jrayga/JasonRayga/master/ASSETS/img/project-moses.jpg",
+      url: "https://www.projectmoses.ph/",
+      shortDesc:
+        "A Crisis Response Toolbox that aims to be an all-in-one platform for Filipinos in times of calamity",
+    },
+  ];
 });
